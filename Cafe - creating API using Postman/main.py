ad39af40bbd7073d6809af931fc5b36c5aa9c812
Abhinav Avasarala <65896914+saiabhi-master@users.noreply.github.com>
2023-05-31 09:18:15 -0400

    Add files via upload

---
+++ Cafe - creating API using Postman/main.py
@@ -0,0 +1,134 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+import random
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+with app.app_context():
+    db.create_all()
+
+def str_to_bool(arg_from_url):
+    if arg_from_url in ['True', ' true', 'T', 't', 'Yes', 'yes', 'y', '1']:
+        return True
+    else:
+        return False
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/random")  #'GET' is allowed by defailt on all routes
+def get_random_cafe():
+    cafes = Cafe.query.all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+@app.route("/all")
+def get_all():
+    cafes = Cafe.query.all()
+    listy = []
+    for cafe in cafes:
+        jsoned = cafe.to_dict()
+        listy.append(jsoned)
+    return jsonify(cafes=listy)
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location=query_location).first()
+    if cafe:
+        return jsonify(cafe=cafe.to_dict())
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+@app.route("/add", methods=["POST"])
+def post_new_cafe(): #request.form and not args because we are using body and params
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+@app.route("/update-price/<int:cafe_id>", methods=["PATCH", "GET"])
+def update_price(cafe_id):
+    req_cafe = Cafe.query.get(cafe_id)
+    price = request.args.get("new_price")
+    print(price)
+    if req_cafe:
+        req_cafe.coffee_price = price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."})
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."})
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE", "GET"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api_key")
+    print(api_key)
+    if api_key == "abhi":
+        cafe = Cafe.query.get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+
+
+
+## HTTP GET - Read Record
+
+## HTTP POST - Create Record
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
